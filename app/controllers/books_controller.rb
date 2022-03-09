327e48cb80d3a779492a397e0a6588c729b2fa0d
EC2 Default User <ec2-user@ip-172-31-6-107.ec2.internal>
2022-03-09 07:13:31 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @book = Book.all
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+  def edit
+     @book = Book.find(params[:id])
+     if @book.user != current_user
+      redirect_to books_path
+     end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'home#top'
+  get 'home/about' => 'home#about', as: :about
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
